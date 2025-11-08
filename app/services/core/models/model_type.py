d3ac577d302b56266ece77bd041d65f251b37f49
jhersbo <jhersbo@gmail.com>
2025-11-08 08:49:53 -0600

    moved model_type again

--- app/services/core/models/model_type.py
+++ app/services/core/models/model_type.py
@@ -1,9 +1,9 @@
 from sqlalchemy import BIGINT, String, JSON, BOOLEAN, select
 from sqlalchemy.orm import Mapped, mapped_column
 
-from ...core.models.entity import Entity
-from ...core.models.globalid import GlobalId
-from ...core.db.session import get_session
+from ....core.models.entity import Entity
+from ....core.models.globalid import GlobalId
+from ....core.db.session import get_session
 
 class ModelType(Entity):
     __tablename__ = "model_type"
