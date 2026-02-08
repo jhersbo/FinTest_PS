7a8fa2e0eb9beb055ed3eacf774d3e3c058bf221
jhersbo <jhersbo@gmail.com>
2026-02-08 11:24:04 -0600

    fixed bug with rq race condition - gid_job_unit was not being saved when accessed in failure callback

--- app/batch/redis_queue.py
+++ app/batch/redis_queue.py
@@ -24,9 +24,7 @@ class RedisQueue:
 
     async def put(self, job:Job) -> rqJob:
         unit = await JobUnit.create()
-        _job = self.Q.enqueue(job.run, args=(unit,), job_timeout=RedisQueue.DEFAULT_TIMEOUT, on_success=end, on_failure=fail)
-        _job.meta["gid_job_unit"] = unit.gid
-        _job.save()
+        _job = self.Q.enqueue(job.run, args=(unit,), job_timeout=RedisQueue.DEFAULT_TIMEOUT, on_success=end, on_failure=fail, meta={"gid_job_unit": unit.gid})
         unit.rq_token = _job.id
         await unit.update()
         return _job
